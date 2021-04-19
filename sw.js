962d8bb025683cfe658f5635e280c5ad6dd8fd49
kevinectv <34521425+kevinectv@users.noreply.github.com>
2021-04-19 17:58:30 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const CACHE_NAME = 'v1_cache_programador_fitness',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
     './style.css',
     './script.js',
-    './images/kevinectv.png',
+    './images/ProgramadorFitness.png',
     './images/favicon.png'
   ]
 
